fdd57ecf185d447095d75a86eab006d618092ce1
Sven Reinck <sreinck@fluxparticle.de>
2024-03-08 15:34:55 +0100

    Lösung - Implementiere die REST API-Schicht

--- handler/CocktailHandler.go
+++ handler/CocktailHandler.go
@@ -1,42 +1,45 @@
 package handler
 
 import (
-	"fmt"
+	"cocktails-go/service"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
 )
 
 type CocktailHandler struct {
+	service *service.CocktailService
 }
 
-func NewCocktailHandler() *CocktailHandler {
-	return &CocktailHandler{}
+func NewCocktailHandler(service *service.CocktailService) *CocktailHandler {
+	return &CocktailHandler{service: service}
 }
 
 func (h *CocktailHandler) CocktailList(c *gin.Context) {
-	panic("implement me")
+	cocktails := h.service.GetAllCocktails()
+	c.JSON(200, cocktails)
 }
 
 func (h *CocktailHandler) CocktailDetails(c *gin.Context) {
 	idStr := c.Param("id")
 	if id, err := strconv.Atoi(idStr); err == nil {
-		fmt.Println("id:", id)
-		panic("implement me")
+		cocktail := h.service.GetCocktailInstructions(uint(id))
+		c.JSON(http.StatusOK, cocktail)
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
 }
 
 func (h *CocktailHandler) IngredientList(c *gin.Context) {
-	panic("implement me")
+	ingredients := h.service.GetAllIngredients()
+	c.JSON(200, ingredients)
 }
 
 func (h *CocktailHandler) IngredientDetails(c *gin.Context) {
 	idStr := c.Param("id")
 	if id, err := strconv.Atoi(idStr); err == nil {
-		fmt.Println("id:", id)
-		panic("implement me")
+		ingredients, cocktails := h.service.GetCocktailsWithIngredients([]uint{uint(id)})
+		c.JSON(http.StatusOK, gin.H{"name": ingredients[0].Name, "cocktails": cocktails})
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
@@ -44,6 +47,8 @@ func (h *CocktailHandler) IngredientDetails(c *gin.Context) {
 
 func (h *CocktailHandler) SearchCocktails(c *gin.Context) {
 	query := c.Query("query")
-	fmt.Println("query:", query)
-	panic("implement me")
+
+	cocktails := h.service.Search(query)
+
+	c.JSON(200, cocktails)
 }
--- repository/Cocktail.go
+++ repository/Cocktail.go
@@ -1,7 +1,7 @@
 package repository
 
 type Cocktail struct {
-	CocktailID   uint `gorm:"primaryKey"`
-	Name         string
-	Instructions []*Instruction `gorm:"foreignKey:CocktailID"`
+	CocktailID   uint           `gorm:"primaryKey" json:"id"`
+	Name         string         `json:"name"`
+	Instructions []*Instruction `gorm:"foreignKey:CocktailID" json:"instructions,omitempty"`
 }
