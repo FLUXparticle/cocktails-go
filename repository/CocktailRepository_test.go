21f80f8dc68b684360bdb79dd96c5cfbac3215d6
Sven Reinck <sreinck@fluxparticle.de>
2024-03-13 13:30:48 +0100

    Lösung - Datenbank testen

--- repository/CocktailRepository_test.go
+++ repository/CocktailRepository_test.go
@@ -7,28 +7,28 @@ import (
 
 func TestNewCocktailRepository(t *testing.T) {
 	RunTest(t, func(repository *CocktailRepository) {
-		// TODO repository ist nicht nil
-		assert.Fail(t, "not implemented")
+		assert.NotNil(t, repository)
 	})
 }
 
 func TestCocktailRepository_FindAll(t *testing.T) {
 	RunTest(t, func(repository *CocktailRepository) {
-		// TODO Es gibt genau 69 Cocktails
-		assert.Fail(t, "not implemented")
+		cocktails := repository.FindAll()
+		assert.Len(t, cocktails, 69)
 	})
 }
 
 func TestCocktailRepository_FindByID(t *testing.T) {
 	RunTest(t, func(repository *CocktailRepository) {
-		// TODO Der Cocktail mit ID 24 heißt "Milkshake" und hat genau 4 Zutaten
-		assert.Fail(t, "not implemented")
+		cocktail := repository.FindByID(24)
+		assert.Equal(t, "Milkshake", cocktail.Name)
+		assert.Len(t, cocktail.Instructions, 4)
 	})
 }
 
 func TestCocktailRepository_FindByNameContains(t *testing.T) {
 	RunTest(t, func(repository *CocktailRepository) {
-		// TODO Es gibt genau 3 Cocktails, deren Namen "Milk" enthält
-		assert.Fail(t, "not implemented")
+		cocktails := repository.FindByNameContains("Milk")
+		assert.Len(t, cocktails, 3)
 	})
 }
--- repository/IngredientRepository_test.go
+++ repository/IngredientRepository_test.go
@@ -7,21 +7,20 @@ import (
 
 func TestNewIngredientRepository(t *testing.T) {
 	RunTest(t, func(repository *IngredientRepository) {
-		// TODO repository ist nit nil
-		assert.Fail(t, "not implemented")
+		assert.NotNil(t, repository)
 	})
 }
 
 func TestIngredientRepository_FindAll(t *testing.T) {
 	RunTest(t, func(repository *IngredientRepository) {
-		// TODO Es gibt genau 61 Zutaten
-		assert.Fail(t, "not implemented")
+		ingredients := repository.FindAll()
+		assert.Len(t, ingredients, 61)
 	})
 }
 
 func TestIngredientRepository_FindByNameContains(t *testing.T) {
 	RunTest(t, func(repository *IngredientRepository) {
-		// TODO Es gibt genau 24 Zutaten deren Namen "sirup" enthält
-		assert.Fail(t, "not implemented")
+		ingredients := repository.FindByNameContains("sirup")
+		assert.Len(t, ingredients, 24)
 	})
 }
